cf49ac2c26df82ae55e3ec36e3bdfad4d5cfa622
Estela Guimarães <97169553+SigmaPink@users.noreply.github.com>
2022-04-22 13:08:16 -0300

    Delete Criar.php

--- Criar.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$senha ="";
-$conexao = pg_connect($servidor, $usuario, $senha) or
-die ("Não foi possível conectar ao servidor PostGreSQL");
-echo "Conexão efetuada com sucesso!!";
-?>
